c55cbbb7e0c363658ca7bdce592bb34ff1a6509f
martin <martin@lena-slyna.slyna.nu>
2017-04-18 13:17:37 +0200

    decent style

--- Main.elm
+++ Main.elm
@@ -1,31 +1,128 @@
+-- Read more about this program in the official Elm guide:
+-- https://guide.elm-lang.org/architecture/effects/http.html
+
 import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Http
+import Json.Decode as Decode exposing (int,string,at)
+import RemoteData as RD exposing (WebData)
+
 
 main =
   Html.program
     { init = init
     , view = view
     , update = update
-    , subscriptions = (\c -> Sub.none)
+    , subscriptions = subscriptions
     }
 
--- model
-type alias Model = {
-  text: String
-}
+
+
+-- MODEL
+
+type alias Tip =
+  { id: Int
+  , text: String
+  , author: String
+  }
+
+type alias Model =
+  { tip: WebData Tip
+  }
+
 
 init : (Model, Cmd Msg)
-init = (Model "hej", Cmd.none)
+init =
+  ( {tip = RD.NotAsked}
+  , getTip
+  )
 
-type Msg = NoOp
 
--- update
+
+-- UPDATE
+
+
+type Msg =
+  NoOp
+  | GetTip
+  | TipResponse (WebData Tip)
+
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
-  (model, Cmd.none)
+  case msg of
+    NoOp ->
+      (model, Cmd.none)
+    GetTip ->
+      (model, getTip)
+    TipResponse tip ->
+      {model | tip = tip} ! []
 
 
--- view
 
+
+-- VIEW
+
+stylesheet =
+    let
+        tag = "link"
+        attrs =
+            [ attribute "rel"       "stylesheet"
+            , attribute "property"  "stylesheet"
+            , attribute "href"      "/mystyle.css"
+            ]
+        children = []
+    in
+        node tag attrs children
+
+
+awesomeTip tipWebdata =
+  case tipWebdata of
+    RD.Success tip ->
+      div [
+        class "wrapper"
+      ] [
+        div [ class "awesomeTip-wrapper"]
+          [
+              div  [ class "autor"] [ text tip.author]
+              ,div [ class "tip-box"] [ text tip.text]
+        ]
+      ]
+
+    _ ->
+      div [] [ text "loading..."]
+
+
+view : Model -> Html Msg
 view model =
-  div [] [ text model.text]
+  div []
+    [ stylesheet
+    , h1 [id "header"] [ text "Vim tip of the day"]
+    , awesomeTip model.tip
+    ]
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+
+
+-- HTTP
+decodeTip : Decode.Decoder Tip
+decodeTip =
+  Decode.map3 Tip
+      (at ["id"] int)
+      (at ["text"] string)
+      (at ["author"] string)
+
+getTip : Cmd Msg
+getTip =
+    Http.get "http://localhost:3000/tips/1" decodeTip
+        |> RD.sendRequest
+        |> Cmd.map TipResponse
